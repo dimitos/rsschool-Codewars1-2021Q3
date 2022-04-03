90182f53b5d205c75cdcc5347ca16eb2e38ea2fd
dimitos <kurort26@mail.ru>
2022-04-03 14:36:23 +0300

    feat: task solution

---
+++ Let's Recycle!.js
@@ -0,0 +1,76 @@
+// Let's Recycle!
+
+/*
+Description.
+
+You will be given a list of objects. Each object has type, material, and possibly secondMaterial. The existing materials are: paper, glass, organic, and plastic.
+Your job is to sort these objects across the 4 recycling bins according to their material (and secondMaterial if it's present), by listing the type's of objects that should go into those bins.
+
+Notes.
+
+The bins should come in the same order as the materials listed above
+All bins should be listed in the output, even if some of them are empty
+If an object is made of two materials, its type should be listed in both of the respective bins
+The order of the type's in each bin should be the same as the order of their respective objects was in the input list
+--------------------------------------------------------------------------
+
+Examples.
+
+input = [
+  {"type": "rotten apples", "material": "organic"},
+  {"type": "out of date yogurt", "material": "organic", "secondMaterial": "plastic"},
+  {"type": "wine bottle", "material": "glass", "secondMaterial": "paper"},
+  {"type": "amazon box", "material": "paper"},
+  {"type": "beer bottle", "material": "glass", "secondMaterial": "paper"}
+]
+
+output = [
+  ["wine bottle", "amazon box", "beer bottle"],
+  ["wine bottle", "beer bottle"],
+  ["rotten apples", "out of date yogurt"],
+  ["out of date yogurt"]
+]
+
+==========================================================================
+*/
+
+function recycle(arr) {
+  const res = { 'organic': [],'paper': [], 'plastic': [], 'glass': [] };
+  for (let key of arr) {
+    res[key.material].push(key.type);
+    if (key.secondMaterial)  res[key.secondMaterial].push(key.type);
+  }
+  return [res['paper'], res['glass'], res['organic'], res['plastic']];
+}
+
+function recycle(arr) {
+  let paper = [], glass = [], organic = [], plastic = [];
+
+  arr.forEach(obj => {
+    if(obj.material === 'paper' || obj.secondMaterial === 'paper' ) paper.push(obj.type)
+    if(obj.material === 'glass' || obj.secondMaterial === 'glass') glass.push(obj.type)
+    if(obj.material === 'organic' || obj.secondMaterial === 'organic') organic.push(obj.type)
+    if(obj.material === 'plastic' || obj.secondMaterial === 'plastic') plastic.push(obj.type)
+  });
+
+  return [paper, glass, organic, plastic]
+}
+
+const recycle = objects => ['paper', 'glass', 'organic', 'plastic'].map(m => objects.filter(o => o.material == m || o.secondMaterial == m).map(o => o.type));
+
+console.log(recycle([
+  {type: 'rotten apples', material: 'organic'},
+  {type: 'out of date yogurt', material: 'organic', secondMaterial: 'plastic'},
+  {type: 'wine bottle', material: 'glass', secondMaterial: 'paper'},
+  {type: 'amazon box', material: 'paper'},
+  {type: 'beer bottle', material: 'glass', secondMaterial: 'paper'}
+]));
+
+
+// output
+// [
+//   ['wine bottle', 'amazon box', 'beer bottle'],
+//   ['wine bottle', 'beer bottle'],
+//   ['rotten apples', 'out of date yogurt'],
+//   ['out of date yogurt']
+// ];
